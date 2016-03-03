297242cbdc34eb2162d273aa4ec7dbfc0f7f8d69
David Beck <dbeck@beckground.hu>
2016-03-03 13:05:55 +0100

    bump version to 0.0.12

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chatter.Mixfile do
   def project do
     [
       app: :chatter,
-      version: "0.0.11",
+      version: "0.0.12",
       elixir: "~> 1.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
